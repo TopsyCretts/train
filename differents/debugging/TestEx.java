6ca866636ba5a368db6a64d8c8a61d4f77f53379
TopsyCretts <topsycrettschannel@gmail.com>
2023-09-20 09:40:16 +0300

    Learned Lesson 150

---
+++ differents/debugging/TestEx.java
@@ -0,0 +1,10 @@
+package differents.debugging;
+
+import java.util.Date;
+
+public class TestEx {
+    public static void main(String[] args) {
+        System.out.println("test");
+        int date = new Date().getDate();
+    }
+}
--- differents/debugging/proxyDebugEx.java
+++ differents/debugging/proxyDebugEx.java
@@ -1,7 +1,63 @@
 package differents.debugging;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.Random;
+import java.util.Scanner;
+
 public class proxyDebugEx {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        method1();
+        method2();
+        method3();
+        method4();
+        //method 5 -> use cmd java TestEx.java > test.log to print all to log file
+        method6();
+        //method 7 -> use cmd java -verbose TestEx.java to get info about all loaded jar and classes
+        //method 8 -> use cmd  javac -Xling:@arg@ TestEx.java check args in png file
+        //method 9 JConsole.exe or JavaProfiler or YourKit or VisualVM
+        Scanner scanner = new Scanner(System.in);
+        while (!scanner.nextLine().endsWith("exit")){
+
+        }
+
 
     }
+    static private void method1(){
+        Random random = new Random(){
+            @Override
+            public double nextDouble() {
+                System.out.println("override method and check whatever you want");
+                return super.nextDouble();
+            }
+        };
+        System.out.println(random.nextDouble());
+    }
+    static private void method2(){
+        try {
+            throw new Exception("exception message");
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    static private void method3(){
+        Thread.dumpStack();
+    }
+    static private void method4() throws FileNotFoundException {
+        PrintWriter printWriter = new PrintWriter(new File("differents\\debugging\\ingest.log"));
+        new Throwable().printStackTrace(printWriter);
+        printWriter.flush();
+        printWriter.close();
+    }
+    static private void method6(){
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                //save to file
+            }
+        });
+    }
+
 }
